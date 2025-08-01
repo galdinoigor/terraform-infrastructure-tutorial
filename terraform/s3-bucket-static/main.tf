2d8e66457c57db46255d9dcd13a3007be819da44
Igor Galdino de Barros Barbosa <ibarbosa@ciandt.com>
2025-08-01 09:21:13 -0300

    Ajustando nome de resource

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
     restrict_public_buckets = false
 }
 
-resource "aws_s3_ownership_controls" "static_site_bucket" {
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
     bucket = aws_s3_bucket.static_site_bucket.id
     rule = {
         object_ownership = "BucketOwnerPreferred"
